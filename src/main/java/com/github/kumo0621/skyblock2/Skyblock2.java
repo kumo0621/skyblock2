e1a04b86dfafa9254edf931587392a290d6f3007
kumo0621 <kumokumokumo0621@gmail.com>
2024-05-14 22:33:58 +0900

    最新

--- src/main/java/com/github/kumo0621/skyblock2/Skyblock2.java
+++ src/main/java/com/github/kumo0621/skyblock2/Skyblock2.java
@@ -1,22 +1,22 @@
 package com.github.kumo0621.skyblock2;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.ComponentSerializer;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.node.Node;
 import net.luckperms.api.node.types.InheritanceNode;
 import net.milkbowl.vault.economy.Economy;
-import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -29,11 +29,8 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public final class Skyblock2 extends JavaPlugin implements Listener {
@@ -45,11 +42,9 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
     private List<String> teams;
     private Team defaultTeam;
     private Team neetTeam;
-    private File playerDataFile;
-    private FileConfiguration playerData;
+
     @Override
     public void onEnable() {
-        createPlayerDataFile();
         // LuckPermsの初期化
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
         if (provider == null) {
@@ -100,69 +95,6 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
                 }
             }
         }, 0L, 20L * 60); // 20 ticks * 60 = 1分ごと
-    }
-
-    @Override
-    public void onDisable() {
-        // プラグインが無効になったときの処理
-        savePlayerData();
-    }
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        // ブロックが置かれたときのイベント
-        if (event.getBlockPlaced().getType() == Material.COBBLESTONE) { // 丸い石を置いた場合
-            String playerName = String.valueOf(event.getPlayer().getUniqueId());
-            int count = playerData.getInt(playerName + ".count", 0); // 現在のカウントを取得
-            playerData.set(playerName + ".count", count + 1); // カウントを1増やす
-            savePlayerData();
-        }
-    }
-    @EventHandler
-    public void onGlassBreak(BlockBreakEvent event) {
-        // ブロックが壊されたときのイベント
-        if (event.getBlock().getType() == Material.COBBLESTONE) { // 丸い石を壊した場合
-            String playerName = String.valueOf(event.getPlayer().getUniqueId());
-            int count = playerData.getInt(playerName + ".count", 0); // 現在のカウントを取得
-            if (count > 0) { // カウントが0より大きい場合のみ減らす
-                playerData.set(playerName + ".count", count - 1); // カウントを1減らす
-                savePlayerData();
-            }
-        }
-    }
-    private void createPlayerDataFile() {
-        playerDataFile = new File(getDataFolder(), "playerData.yml");
-        if (!playerDataFile.exists()) {
-            playerDataFile.getParentFile().mkdirs();
-            saveResource("playerData.yml", false);
-        }
-        playerData = YamlConfiguration.loadConfiguration(playerDataFile);
-    }
-    public int getPlayerData(String playerName) {
-        return playerData.getInt(playerName + ".count", 0);
-    }
-    private void savePlayerData() {
-        try {
-            playerData.save(playerDataFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //ツールでの破壊系
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        Block block = event.getBlock();
-        Material toolType = event.getPlayer().getInventory().getItemInMainHand().getType();
-        GameMode gameMode = event.getPlayer().getGameMode();
-        if (gameMode == GameMode.CREATIVE) {
-            return;
-        }
-
-        if (block.getType() == Material.SANDSTONE) {
-            event.setCancelled(true);
-
-            return;
-        }
     }
 
     /**
@@ -249,6 +181,9 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
             player.performCommand("warp");
         }
 
+        // LuckPermsのグループを設定
+        setGroup(player, team.getName());
+
         // コンパスを持っていない場合、コンパスを与える
         if (!hasCompass(player)) {
             ItemStack compass = new ItemStack(Material.COMPASS);
@@ -257,8 +192,6 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
             // ここでコンパスに名前を設定します
             meta.setDisplayName("右クリックでテレポート");
             meta.setLore(Arrays.asList(
-                    "右クリックでメイン職業のアドミンショップにテレポート",
-                    "左クリックでメイン職業を切り替え",
                     "しゃがみ右クリックで残高を確認",
                     "しゃがみ左クリックでNotionを確認"
             ));
@@ -288,50 +221,32 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        // 職業追加
-        if (cmd.getName().equalsIgnoreCase("addrole")) {
+        // 職業設定コマンドの処理
+        if (cmd.getName().equalsIgnoreCase("setrole")) {
             // コマンド実行者とコマンドの引数から、対象のプレイヤーを取得
             List<Player> players = getTargetPlayer(sender, args, 1);
             if (players == null) return false;
+
             // コンフィグに職業が存在しない場合のみ処理を続ける
             for (Player player : players) {
-                // LuckPermsのグループを取得
-                getGroup(player).thenAccept(groups -> {
-                    if (groups.size() < getConfig().getInt("maxRole")) {
-                        if (args.length >= 1) {
-                            String role = args[0];
-                            List<String> newGroups = new ArrayList<>(groups);
-                            newGroups.add(role);
-
-                            // LuckPermsのグループを設定
-                            setGroup(player, newGroups).thenAccept(result -> {
-                                if (result) {
-                                    player.sendMessage(ChatColor.GREEN + "あなたの職業が追加されました: " + role);
-
-                                } else {
-                                    player.sendMessage(ChatColor.RED + "無効な職業です。");
-                                }
-                            });
+                if (neetTeam.hasPlayer(player) || sender.hasPermission("skyblock2.admin")) {
+                    if (args.length >= 1) {
+                        String role = args[0];
+                        if (setRole(player, role)) {
+                            player.sendMessage(ChatColor.GREEN + "職業が設定されました: " + role);
+                            return true;
                         } else {
-                            player.sendMessage(ChatColor.RED + "使用方法: /setrole <職業名>");
+                            player.sendMessage(ChatColor.RED + "無効な職業です。");
+                            return false;
                         }
                     } else {
-                        player.sendMessage(ChatColor.RED + "あなたは既に職業を持っています。");
+                        player.sendMessage(ChatColor.RED + "使用方法: /setrole <職業名>");
+                        return false;
                     }
-                });
-            }
-        }
-        // 職業クリア
-        if (cmd.getName().equalsIgnoreCase("clearrole")) {
-            // コマンド実行者とコマンドの引数から、対象のプレイヤーを取得
-            List<Player> players = getTargetPlayer(sender, args, 0);
-            if (players == null) return false;
-            // コンフィグに職業が存在しない場合のみ処理を続ける
-            for (Player player : players) {
-                // LuckPermsのグループを設定
-                setGroup(player, Collections.emptyList()).thenAccept(result -> {
-                    player.sendMessage(ChatColor.GREEN + "職業がクリアされました");
-                });
+                } else {
+                    player.sendMessage(ChatColor.RED + "あなたは既に職業を持っています。");
+                    return true;
+                }
             }
         }
 
@@ -366,44 +281,13 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
             }
         }
 
-        // ジョブチェンジコマンドの処理
-        if (cmd.getName().equalsIgnoreCase("jobchange")) {
-            // コマンド実行者とコマンドの引数から、対象のプレイヤーを取得
-            List<Player> players = getTargetPlayer(sender, args, 0);
-            if (players == null) return false;
-
-            for (Player player : players) {
-                Team team = board.getPlayerTeam(player);
-                // LuckPermsのグループを取得
-                getGroup(player).thenAccept(groups -> {
-                    // グループ数が0個はエラー
-                    if (groups.size() == 0) {
-                        player.sendMessage(ChatColor.RED + "あなたは職業を持っていません。");
-                        return;
-                    }
-
-                    // 自分のチームの次の職業を取得
-                    int index = groups.indexOf(team.getName());
-                    if (index == -1) index = 0;
-                    index = (index + 1) % groups.size();
-
-                    // チームを設定
-                    String role = groups.get(index);
-                    setTeam(player, role);
-                    // チャットにメッセージを表示
-                    player.sendMessage(ChatColor.GREEN + "メイン職業が変更されました: " + role);
-                });
-            }
-        }
-
         // Notionコマンドの処理
-        if (cmd.getName().equalsIgnoreCase("notion")) {
+        if (cmd.getName().equalsIgnoreCase("notion") && sender instanceof Player) {
             Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + sender.getName() + " " + getConfig().getString("notion"));
         }
 
         return false;
     }
-
 
     /**
      * コマンド実行者とコマンドの引数から、対象のプレイヤーを取得する
@@ -441,34 +325,37 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
      *
      * @param player プレイヤー
      * @param role   職業
+     * @return 設定に成功したかどうか
      */
-    public void setTeam(Player player, String role) {
-        Bukkit.getScheduler().runTask(this, () -> {
-            // 引数の文字列のチームを取得
-            Team team = board.getTeam(role);
-            // チームが存在すればプレイヤーを追加
-            if (team != null) {
-                team.addEntry(player.getName());
-            }
-        });
+    public boolean setRole(Player player, String role) {
+        // 引数の文字列のチームを取得
+        Team team = board.getTeam(role);
+        // チームが存在すればプレイヤーを追加
+        if (team != null) {
+            team.addEntry(player.getName());
+
+            // LuckPermsのグループを設定
+            setGroup(player, role);
+
+            return true;
+        }
+        return false;
     }
 
     /**
      * LuckPermsのグループを設定
      *
      * @param player プレイヤー
-     * @param roles  職業
+     * @param role   職業
      */
-    private CompletableFuture<Boolean> setGroup(Player player, List<String> roles) {
+    private void setGroup(Player player, String role) {
         // プレイヤー情報を読み込み
-        return luckPerms.getUserManager().loadUser(player.getUniqueId()).thenApply(user -> {
+        luckPerms.getUserManager().loadUser(player.getUniqueId()).thenAccept(user -> {
             // プレイヤーに職業のグループを付与
-            boolean result = false;
             for (String groupName : teams) {
-                if (roles.contains(groupName)) {
+                if (groupName.equals(role)) {
                     // プレイヤーが所属しているグループに職業のグループを追加
                     user.data().add(InheritanceNode.builder(groupName).value(true).build());
-                    result = true;
                 } else {
                     // プレイヤーが所属しているグループ以外のグループを削除
                     user.data().remove(InheritanceNode.builder(groupName).build());
@@ -476,18 +363,7 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
             }
             // プレイヤー情報を保存
             luckPerms.getUserManager().saveUser(user);
-            return result;
         });
-    }
-
-    private CompletableFuture<List<String>> getGroup(Player player) {
-        // プレイヤー情報を読み込み
-        return luckPerms.getUserManager().loadUser(player.getUniqueId())
-                .thenApply(user -> user.data().toCollection().stream()
-                        .filter(Node::getValue)
-                        .map(node -> node.getKey().substring("group.".length()))
-                        .filter(teams::contains)
-                        .collect(Collectors.toList()));
     }
 
     @EventHandler
@@ -511,9 +387,6 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
                 if (player.isSneaking()) {
                     // コンパスをしゃがみ左クリックしたときの処理
                     player.performCommand("notion");
-                } else {
-                    // コンパスを左クリックしたときの処理
-                    //player.performCommand("jobchange");
                 }
             }
         }
@@ -522,25 +395,13 @@ public final class Skyblock2 extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
-        String playerName = String.valueOf(player.getUniqueId());
+
         // 罰金徴収
-        int penalty = getPlayerData(playerName)*10;
+        int penalty = getConfig().getInt("deathPenalty");
         if (econ.withdrawPlayer(player, penalty).transactionSuccess()) {
-            player.sendMessage(ChatColor.RED + "死亡により土地の税金￥" + penalty + "徴収されました。");
+            player.sendMessage(ChatColor.RED + "死亡により￥" + penalty + "徴収されました。");
         } else {
             player.sendMessage(ChatColor.RED + "死亡しましたが、お金が足りないため徴収されませんでした。");
-        }
-    }
-    @EventHandler
-    public void onCobolStone(BlockPlaceEvent event) {
-        // ブロックが置かれるイベントを取得
-        Block blockAbove = event.getBlockAgainst(); // 置かれようとしているブロックの上のブロック
-        Block blockPlaced = event.getBlockPlaced(); // 置かれようとしているブロック
-
-        // もし置かれようとしているブロックの下が草ブロックで、置かれようとしているブロックが丸い石ではない場合
-        if (blockAbove.getType() == Material.SANDSTONE && blockPlaced.getType() != Material.COBBLESTONE) {
-            event.setCancelled(true); // イベントをキャンセルし、ブロックの設置を禁止する
-            event.getPlayer().sendMessage("コンクリの上には丸い石しか置けません。");
         }
     }
 }
